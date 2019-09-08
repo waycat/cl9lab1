4b9a461d55a69a5a9fe2d03cf17d7f112280ae08
Vera Michkovsky <vera.michkovsky@hotmail.com>
2019-09-08 19:04:57 +0300

    implement

--- cl9lab1.cpp
+++ cl9lab1.cpp
@@ -7,6 +7,17 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	int perimeter, square;
+	int width = 0, length = 0;
+	cout << "input width: ";
+
+	cin >> width;
+	cout << "input length: ";
+	cin >> length;
+	perimeter = (width + length) * 2;
+	cout << "perimeter is: " << perimeter << endl;
+	square = width * length;
+	cout << "square is: " << square << endl;
+
 	return 0;
 }
